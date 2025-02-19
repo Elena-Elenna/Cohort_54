996311b6beed7ca696cc52c5f5edf5678b47a477
ElenaH <info.elenna@gmail.com>
2025-02-19 22:09:58 +0100

    HW_39

---
+++ src/homework_39/Task_1_2.java
@@ -0,0 +1,80 @@
+package homework_39;
+/*
+
+Task 1
+Частотный словарь слов: Напишите метод, который принимает строку текста и возвращает Map<String, Integer>,
+где каждому слову соответствует количество его вхождений в текст.
+
+Task2 (*Опционально):
+Напишите метод, возвращающий частотный словарь вхождения символов в строку
+
+public class FrequencyDictionary {
+    public static void main(String[] args) {
+        String text = "Строка для проверки количества вхождений слов" +
+                ".\n Строка для проверки и теста. и количества слов для проверки";
+
+        Map<String, Integer> frequencyMap = frequencyDictionary(text);
+        frequencyMap.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        Map<Character, Integer> frequencyCharsMap = frequencyCharsDictionary(text);
+
+        System.out.println("\nВхождение символов: ");
+        frequencyCharsMap.forEach((k, v) -> System.out.print("'" + k + "': " + v+ "; "));
+        System.out.println();
+    }
+Copy
+//Output:
+Строка: 2
+для: 3
+проверки: 3
+количества: 2
+вхождений: 1
+слов: 2
+и: 2
+теста: 1
+
+Вхождение символов:
+'
+': 1; ' ': 15; '.': 2; 'С': 2; 'а': 5; 'в': 8; 'д': 4; 'е': 7; 'ж': 1; 'и': 8; 'й': 1; 'к': 7; 'л': 7;
+'н': 1; 'о': 10; 'п': 3; 'р': 8; 'с': 5; 'т': 6; 'х': 1; 'ч': 2; 'я': 3;
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_1_2 {
+    public static void main(String[] args) {
+        String text = "Строка для проверки количества вхождений слов" +
+                ".\n Строка для проверки и теста. и количества слов для проверки";
+
+        Map<String, Integer> frequencyMap = frequencyDictionary(text);
+        frequencyMap.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Map<Character, Integer> frequencyCharsMap = frequencyCharsDictionary(text);
+
+        System.out.println("\nВхождение символов: ");
+        frequencyCharsMap.forEach((k, v) -> System.out.print("'" + k + "': " + v+ "; "));
+        System.out.println();
+    }
+
+
+    private static Map<String, Integer> frequencyDictionary(String text) {
+        String[] strings = text.trim().replaceAll("[^a-zA-Zа-яА-Я ]", "").split("\\s+");
+        Map<String, Integer> map = new HashMap<>();
+        for (String key : strings) {
+            Integer value = map.getOrDefault(key, 0);
+            map.put(key, ++value);
+        }
+        return map;
+    }
+
+    private static Map<Character, Integer> frequencyCharsDictionary(String text) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char key : text.toCharArray()) {
+            Integer value = map.getOrDefault(key, 0);
+            map.put(key, ++value);
+        }
+        return map;
+    }
+}
